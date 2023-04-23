1557518f3fb85c2fdce4a1f2f29d2b0c1305acf3
Marine Ramillon <marine.ramillon@gmail.com>
2023-04-23 19:32:33 +0200

    add router and views folder

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<h1>hih</h1>')
-});
+const shopRouter = require('./routes/shop')
+const adminRoutes = require('./routes/admin')
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello</h1>')
-});
+app.use(express.urlencoded({extended: true}));
 
+app.use('/admin', adminRoutes)
+app.use(shopRouter)
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000)
