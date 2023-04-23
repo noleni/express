50d0d90fce4187ae240fe8ccbb4d10be1f6ecb5e
Marine Ramillon <marine.ramillon@gmail.com>
2023-04-23 16:52:58 +0200

    add gitignore file

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<h1>hih</h1>')
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello</h1>')
+});
+
+
+app.listen(3000)
