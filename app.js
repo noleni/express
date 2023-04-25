ce2159d7d9017c67d2d0afa15f7743a1e405dce8
Marine Ramillon <marine.ramillon@gmail.com>
2023-04-25 18:08:32 +0200

    replacing html by pug files

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const rootDir = require('./helper/path')
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 const shopRouter = require('./routes/shop')
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({extended: true}));
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRouter)
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404error.html'))
+  res.status(404).render('404')
 })
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,17 @@ const rootDir = require('../helper/path')
 
 const router = express.Router();
 
+const products = [];
+
 
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+  res.render('add-product', {pageTtitle : "Add Product", path : '/admin/add-product'})
 });
 
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body)
+  products.push({title : req.body.title})
   res.redirect('/')
 })
 
-module.exports = router
+exports.routes = router;
+exports.products = products;
